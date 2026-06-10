5d7c0242a328535e8775a1d9e00666585297a530
Danny Xu <xuyanrong@icloud.com>
2026-06-09 22:11:48 -0400

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,11 @@
+def add_two_numbers() -> int:
+    line = input()
+    strings = line.split(",")
+    return int(strings[0]) + int(strings[1])
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
